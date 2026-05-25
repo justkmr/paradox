eb02827bf7294a09d41595be8dcc6b5ecf79ac65
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-05-25 19:28:03 +0530

    updated preloader page

--- src/components/loaders/Preloader.tsx
+++ src/components/loaders/Preloader.tsx
@@ -831,7 +831,7 @@ export default function Preloader({ onComplete }: { onComplete: () => void }) {
             </AnimatePresence>
           </div>
 
-          <div className="w-full flex items-center justify-between border-t border-white/[0.04] pt-4.5">
+          <div className="w-full flex items-center justify-between border-t border-white/4 pt-4.5">
             <div className="flex items-center gap-1.5">
               {[...Array(5)].map((_, i) => (
                 <div
