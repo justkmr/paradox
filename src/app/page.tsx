13e3a17808f4c32d0ae1c2a51c84103e3616cac6
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-05-25 19:05:42 +0530

    updated  page with better loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import About from "@/components/sections/About";
 export default function Home() {
   return (
     <div className="relative min-h-screen w-full flex flex-col isolation-isolate bg-[#080808] text-[#f0f0f0]">
-      {/* Absolute Master Grid Sync Layer */}
       <div className="grid-bg fixed inset-0 z-0 pointer-events-none opacity-40" />
 
       <Navbar />
