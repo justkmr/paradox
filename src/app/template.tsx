6c7a08b7d1fd16f701cd881294d9aed2cc6e4937
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-05-26 21:54:12 +0530

    added template

---
+++ src/app/template.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      initial={{ y: 20, opacity: 0, filter: "blur(10px)" }}
+      animate={{ y: 0, opacity: 1, filter: "blur(0px)" }}
+      transition={{
+        duration: 0.6,
+        ease: [0.22, 1, 0.36, 1], // Elite Cubic Bezier Easing
+      }}
+      className="w-full flex-grow flex flex-col"
+    >
+      {children}
+    </motion.div>
+  );
+}
