8d1e48a6e156da1295b22509f2af24e3634be2b0
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-05-27 22:45:54 +0530

    added full paradox

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // 🌟 IMAGES HOSTNAME CONFIGURATION ADD KIYA
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.simpleicons.org",
+        port: "",
+        pathname: "/**", // Isse simpleicons ke saare paths whitelist ho jayenge
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,19 +1,19 @@
-"use client";
+// "use client";
 
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
-export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <motion.div
-      initial={{ y: 20, opacity: 0, filter: "blur(10px)" }}
-      animate={{ y: 0, opacity: 1, filter: "blur(0px)" }}
-      transition={{
-        duration: 0.6,
-        ease: [0.22, 1, 0.36, 1],
-      }}
-      className="w-full grow flex flex-col"
-    >
-      {children}
-    </motion.div>
-  );
-}
+// export default function Template({ children }: { children: React.ReactNode }) {
+//   return (
+//     <motion.div
+//       initial={{ y: 20, opacity: 0, filter: "blur(10px)" }}
+//       animate={{ y: 0, opacity: 1, filter: "blur(0px)" }}
+//       transition={{
+//         duration: 0.6,
+//         ease: [0.22, 1, 0.36, 1],
+//       }}
+//       className="w-full grow flex flex-col"
+//     >
+//       {children}
+//     </motion.div>
+//   );
+// }
