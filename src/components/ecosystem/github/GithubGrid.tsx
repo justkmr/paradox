eb337908b92379f3bef282a4f32c1410eafc808b
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-05-30 18:35:17 +0530

    updated github grid card

--- src/components/ecosystem/github/GithubGrid.tsx
+++ src/components/ecosystem/github/GithubGrid.tsx
@@ -217,7 +217,7 @@ export function GithubGrid() {
             </div>
 
             {/* 💎 FUTURISTIC TERMINAL LOG-STYLE TECH TAGS */}
-            <div className="flex flex-wrap gap-2 relative z-10 w-full pt-2 border-t border-white/[0.03]">
+            <div className="flex flex-wrap gap-2 relative z-10 w-full pt-2 border-t border-white/3">
               {repo.tags.map((tag) => (
                 <span
                   key={tag}
