50b9bf820d463a080207a9b4f99243343cdb4cde
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-05-31 16:37:00 +0530

    updated github card

--- src/components/ecosystem/github/GithubCard.tsx
+++ src/components/ecosystem/github/GithubCard.tsx
@@ -30,7 +30,7 @@ export function GithubCard({ data }: { data: ProfileData }) {
             <span className="w-1.5 h-1.5 rounded-full bg-[#E24C60] shadow-[0_0_8px_#E24C60] animate-pulse" />
             <span>Core Hub Active</span>
           </div>
-          <span className="text-[10px] bg-[#E24C60] p-1 font-black rounded-full  tracking-widest uppercase shadow-[0_0_8px_#E24C60] ">
+          <span className="text-[10px] bg-[#E24C60] p-1 w-fit h-fit font-black rounded-full  tracking-widest uppercase shadow-[0_0_8px_#E24C60] ">
             Terminal v1.0
           </span>
         </div>
