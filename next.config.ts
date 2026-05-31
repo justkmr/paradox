97cc183ded598d844671c2db56fc707400a7d1f3
Pintu-Kumar-1709 <kumarpintu50644@gmail.com>
2026-05-31 16:53:50 +0530

    updated next configuration package file

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cdn.simpleicons.org",
         port: "",
-        pathname: "/**", // Isse simpleicons ke saare paths whitelist ho jayenge
+        pathname: "/**", 
       },
       {
         protocol: "https",
