263b5772f1ab0156d73d0128365f25e9f03383c8
justkmr <kumarpintu50644@gmail.com>
2026-06-19 15:58:37 +0530

    updated scroll to top button

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -30,7 +30,7 @@ export default function ScrollToTop() {
           whileHover={{ scale: 1.08 }}
           whileTap={{ scale: 0.92 }}
           className="
-          fixed bottom-8 right-8 z-[999999]
+          fixed bottom-8 right-8 z-999999
 w-14 h-14 rounded-full
 bg-[#0b0b0d]/90
 backdrop-blur-xl
